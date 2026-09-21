9143daee3de2c352a3125bb05930bd11a2cf79b7
PacaYvonne <yvonne@pacagen.com>
2026-09-20 22:00:57 -0400

    feat: whop pixel

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -1,3 +1,7 @@
+!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");
+whop.setScope("biz_ZWtfEcddSAJb77");
+whop.track("page");
+
 (function() {
 
     var SESSION_PV_COUNT_KEY = 'cvg_session_pv_count';
